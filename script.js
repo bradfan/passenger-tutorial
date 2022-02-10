ddb9e23b64cf056019f3a41f5d526a01320a2341
bradfan <bradley.tomlinson@genspark.com>
2022-02-10 09:33:12 -0700

    "force commit"

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+let sum = document.getElementById("sum");
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 let count = 0;
+let total = 0;
+
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
+
+function save() {
+  let countStr = count  + " - ";
+  saveEl.textContent += countStr;
+  total += count;
+  sum.textContent = "Running Total: " + total; 
+  countEl.textContent = 0;
+  count = 0;
+}
+
+
